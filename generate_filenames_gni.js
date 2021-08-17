71e554279871746a19943dc1eef9690ffeb958be
Cheng Zhao <zcbenz@gmail.com>
2021-08-17 10:16:09 +0900

    Use jumbo build for absl

---
+++ generate_filenames_gni.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const {searchFiles} = require('../../scripts/common')
+
+const fs = require('fs')
+
+process.chdir('third_party/abseil-cpp')
+const files = searchFiles('absl', ['.cc', '.h']).sort()
+const sources = files.filter(shouldInclude)
+const internals = sources.filter(s => s.includes('internal'))
+const externals = sources.filter(s => !s.includes('internal'))
+
+const inc_files = searchFiles('absl', '.inc')
+
+fs.writeFileSync('filenames.gni',
+                 'absl_inc_files = ' + toGNArray(inc_files) + '\n' +
+                 'absl_internals = ' + toGNArray(internals) + '\n' +
+                 'absl_externals = ' + toGNArray(externals))
+
+function shouldInclude(f) {
+  return !f.includes('test') &&
+         !f.includes('benchmark') &&
+         !f.includes('/random/') &&
+         !f.endsWith('/bad_any_cast.cc') &&
+         !f.endsWith('/print_hash_of.cc')
+}
+
+function toGNArray(a) {
+  const arr = a.map(s => '  "' + s + '",\n')
+  return '[\n' + arr.join('') + ']\n'
+}
